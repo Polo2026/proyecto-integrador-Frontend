9ae4d0daea6f00a041289b9fb2e3ced4489d4d1e
Cristian polo <polocristian1896@gmail.com>
2026-08-26 18:14:31 -0500

    avance

---
+++ inicio-sesion.js
@@ -0,0 +1,19 @@
+//Solicitar al usuario el ingreso del nombre de usuario y la contraseña.
+
+
+//Leer e imprimir los datos por consola.
+
+
+//Verificar mediante condicionales si las credenciales son correctas.
+
+
+//Permitir el acceso cuando los datos sean válidos.
+
+
+//Indicar cuando los datos sean incorrectos.
+
+
+//Utilizar un ciclo para controlar los intentos de acceso.
+
+
+//Mostrar un mensaje de bloqueo cuando se superen tres intentos fallidos
