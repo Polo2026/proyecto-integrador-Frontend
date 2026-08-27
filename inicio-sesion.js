69e716b7b494072c39789ffac9608773321780c3
Seleny <Selenymeza565@gmail.com>
2026-08-26 20:01:33 -0500

    Ejercicio 1

--- inicio-sesion.js
+++ inicio-sesion.js
@@ -1,4 +1,13 @@
 //Solicitar al usuario el ingreso del nombre de usuario y la contraseña.
+function iniciarSesion() {
+  let usuario = prompt("Ingrese su nombre de usuario:");
+  let contrasena = prompt("Ingrese su contraseña:");
+
+  console.log("Usuario ingresado: " + usuario);
+  console.log("Contraseña ingresada: " + contrasena);
+}
+
+iniciarSesion();
 
 
 //Leer e imprimir los datos por consola.
@@ -21,3 +30,8 @@
 
 
 //Hola Mundo
+
+
+
+//Hola soy Seleny
+
